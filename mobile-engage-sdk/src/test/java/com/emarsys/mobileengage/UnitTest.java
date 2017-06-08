286c3ce10ca14c20e2ceb96839b474924bed273f
jpollak <jozsef.pollak@emarsys.com>
2017-06-08 14:31:22 +0200

    test(unit-test): add empty unit test

---
+++ mobile-engage-sdk/src/test/java/com/emarsys/mobileengage/UnitTest.java
@@ -0,0 +1,12 @@
+package com.emarsys.mobileengage;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class UnitTest {
+    @Test
+    public void test(){
+        Assert.assertTrue(true);
+    }
+}
