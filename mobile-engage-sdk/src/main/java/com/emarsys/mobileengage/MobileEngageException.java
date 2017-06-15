aa45b1719db77342f003a3a7718774eda86c98fd
jpollak <jozsef.pollak@emarsys.com>
2017-06-15 14:46:32 +0200

    feat(mobile-engage-exception): add secondary exception

--- mobile-engage-sdk/src/main/java/com/emarsys/mobileengage/MobileEngageException.java
+++ mobile-engage-sdk/src/main/java/com/emarsys/mobileengage/MobileEngageException.java
@@ -1,5 +1,7 @@
 package com.emarsys.mobileengage;
 
+import com.emarsys.core.response.ResponseModel;
+
 public class MobileEngageException extends Exception {
     private final int statusCode;
     private final String statusMessage;
@@ -10,6 +12,10 @@ public class MobileEngageException extends Exception {
         this.statusCode = statusCode;
         this.statusMessage = statusMessage;
         this.body = body;
+    }
+
+    public MobileEngageException(ResponseModel requestModel) {
+        this(requestModel.getStatusCode(), requestModel.getMessage(), requestModel.getBody());
     }
 
     public int getStatusCode() {
