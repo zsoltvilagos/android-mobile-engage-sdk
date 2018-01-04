20f35ce8b4f6a7209641ba8eef9050f44b39f06c
jpollak <jozsef.pollak@emarsys.com>
2018-01-04 11:02:44 +0100

    fix(experimental): use US locale with toLowerCase

--- mobile-engage-sdk/src/main/java/com/emarsys/mobileengage/experimental/MobileEngageFeature.java
+++ mobile-engage-sdk/src/main/java/com/emarsys/mobileengage/experimental/MobileEngageFeature.java
@@ -1,11 +1,13 @@
 package com.emarsys.mobileengage.experimental;
 
+import java.util.Locale;
+
 public enum MobileEngageFeature implements FlipperFeature {
 
     IN_APP_MESSAGING;
 
     @Override
     public String getName() {
-        return name().toLowerCase();
+        return name().toLowerCase(Locale.US);
     }
 }
