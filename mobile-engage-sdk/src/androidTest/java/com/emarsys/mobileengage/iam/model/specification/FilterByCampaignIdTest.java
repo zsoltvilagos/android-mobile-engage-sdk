424d4c6cff804008d940b350dd4cea6bb2559fc4
jpollak <jozsef.pollak@emarsys.com>
2018-02-05 15:19:52 +0100

    feat(filter-by-campaign-id): enable batch filtering

--- mobile-engage-sdk/src/androidTest/java/com/emarsys/mobileengage/iam/model/specification/FilterByCampaignIdTest.java
+++ mobile-engage-sdk/src/androidTest/java/com/emarsys/mobileengage/iam/model/specification/FilterByCampaignIdTest.java
@@ -2,10 +2,13 @@ package com.emarsys.mobileengage.iam.model.specification;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.filters.SdkSuppress;
 
 import com.emarsys.core.database.repository.specification.QueryAll;
+import com.emarsys.mobileengage.iam.model.buttonclicked.ButtonClicked;
+import com.emarsys.mobileengage.iam.model.buttonclicked.ButtonClickedContract;
+import com.emarsys.mobileengage.iam.model.buttonclicked.ButtonClickedRepository;
 import com.emarsys.mobileengage.iam.model.displayediam.DisplayedIam;
+import com.emarsys.mobileengage.iam.model.displayediam.DisplayedIamContract;
 import com.emarsys.mobileengage.iam.model.displayediam.DisplayedIamRepository;
 import com.emarsys.mobileengage.testUtil.DatabaseTestUtils;
 import com.emarsys.mobileengage.testUtil.TimeoutUtils;
@@ -16,17 +19,13 @@ import org.junit.Test;
 import org.junit.rules.TestRule;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static android.os.Build.VERSION_CODES.KITKAT;
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
-@SdkSuppress(minSdkVersion = KITKAT)
 public class FilterByCampaignIdTest {
 
-    String campaignId;
-    FilterByCampaignId specification;
     private Context context;
 
     @Rule
@@ -36,43 +35,131 @@ public class FilterByCampaignIdTest {
     public void init() {
         DatabaseTestUtils.deleteMobileEngageDatabase();
 
-        campaignId = "campaign2";
-        specification = new FilterByCampaignId(campaignId);
         context = InstrumentationRegistry.getContext();
     }
 
     @Test
-    public void getSql() throws Exception {
-        String expected = "campaign_id=?";
-        String result = specification.getSql();
-        assertEquals(expected, result);
-    }
-
-    @Test
-    public void getArgs() throws Exception {
-        String[] expected = new String[]{campaignId};
-        String[] result = specification.getArgs();
-        assertArrayEquals(expected, result);
-    }
-
-    @Test
-    public void testSpecification_shouldWorkAsIntended() {
+    public void testExecution_displayedIam_shouldDeleteIam() {
         DisplayedIamRepository repository = new DisplayedIamRepository(context);
 
         DisplayedIam iam1 = new DisplayedIam("campaign1", 10L);
         DisplayedIam iam2 = new DisplayedIam("campaign2", 20L);
         DisplayedIam iam3 = new DisplayedIam("campaign3", 30L);
-        DisplayedIam iam4 = new DisplayedIam("campaign2", 40L);
+
+        repository.add(iam1);
+        repository.add(iam2);
+        repository.add(iam3);
+
+        repository.remove(new FilterByCampaignId("campaign2"));
+
+        List<DisplayedIam> result = repository.query(new QueryAll(DisplayedIamContract.TABLE_NAME));
+        List<DisplayedIam> expected = Arrays.asList(iam1, iam3);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testExecution_displayedIam_shouldDelete_multipleIams() {
+        DisplayedIamRepository repository = new DisplayedIamRepository(context);
+
+        DisplayedIam iam1 = new DisplayedIam("campaign1", 10L);
+        DisplayedIam iam2 = new DisplayedIam("campaign2", 20L);
+        DisplayedIam iam3 = new DisplayedIam("campaign3", 30L);
+        DisplayedIam iam4 = new DisplayedIam("campaign4", 40L);
 
         repository.add(iam1);
         repository.add(iam2);
         repository.add(iam3);
         repository.add(iam4);
 
-        repository.remove(specification);
+        repository.remove(new FilterByCampaignId("campaign1", "campaign2"));
 
-        List<DisplayedIam> result = repository.query(new QueryAll("displayed_iam"));
-        List<DisplayedIam> expected = Arrays.asList(iam1, iam3);
+        List<DisplayedIam> result = repository.query(new QueryAll(DisplayedIamContract.TABLE_NAME));
+        List<DisplayedIam> expected = Arrays.asList(iam3, iam4);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testExecution_displayedIam_withEmptyIdArray() {
+        DisplayedIamRepository repository = new DisplayedIamRepository(context);
+
+        DisplayedIam iam1 = new DisplayedIam("campaign1", 10L);
+        DisplayedIam iam2 = new DisplayedIam("campaign2", 20L);
+        DisplayedIam iam3 = new DisplayedIam("campaign3", 30L);
+        DisplayedIam iam4 = new DisplayedIam("campaign4", 40L);
+
+        repository.add(iam1);
+        repository.add(iam2);
+        repository.add(iam3);
+        repository.add(iam4);
+
+        repository.remove(new FilterByCampaignId());
+
+        List<DisplayedIam> result = repository.query(new QueryAll(DisplayedIamContract.TABLE_NAME));
+        List<DisplayedIam> expected = Arrays.asList(iam1, iam2, iam3, iam4);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testExecution_buttonClicked_shouldDeleteIam() {
+        ButtonClickedRepository repository = new ButtonClickedRepository(context);
+
+        ButtonClicked btn1 = new ButtonClicked("campaign1", "button1", 10L);
+        ButtonClicked btn2 = new ButtonClicked("campaign1", "button3", 10L);
+        ButtonClicked btn3 = new ButtonClicked("campaign2", "button10", 10L);
+
+        repository.add(btn1);
+        repository.add(btn2);
+        repository.add(btn3);
+
+        repository.remove(new FilterByCampaignId("campaign2"));
+
+        List<ButtonClicked> result = repository.query(new QueryAll(ButtonClickedContract.TABLE_NAME));
+        List<ButtonClicked> expected = Arrays.asList(btn1, btn2);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testExecution_buttonClicked_shouldDelete_multipleIams() {
+        ButtonClickedRepository repository = new ButtonClickedRepository(context);
+
+        ButtonClicked btn1 = new ButtonClicked("campaign1", "button1", 10L);
+        ButtonClicked btn2 = new ButtonClicked("campaign1", "button3", 10L);
+        ButtonClicked btn3 = new ButtonClicked("campaign2", "button10", 10L);
+        ButtonClicked btn4 = new ButtonClicked("campaign3", "button10", 10L);
+
+        repository.add(btn1);
+        repository.add(btn2);
+        repository.add(btn3);
+        repository.add(btn4);
+
+        repository.remove(new FilterByCampaignId("campaign1", "campaign2"));
+
+        List<ButtonClicked> result = repository.query(new QueryAll(ButtonClickedContract.TABLE_NAME));
+        List<ButtonClicked> expected = Collections.singletonList(btn4);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testExecution_buttonClicked_withEmptyIdArray() {
+        ButtonClickedRepository repository = new ButtonClickedRepository(context);
+
+        ButtonClicked btn1 = new ButtonClicked("campaign1", "button1", 10L);
+        ButtonClicked btn2 = new ButtonClicked("campaign1", "button3", 10L);
+        ButtonClicked btn3 = new ButtonClicked("campaign2", "button10", 10L);
+
+        repository.add(btn1);
+        repository.add(btn2);
+        repository.add(btn3);
+
+        repository.remove(new FilterByCampaignId());
+
+        List<ButtonClicked> result = repository.query(new QueryAll(ButtonClickedContract.TABLE_NAME));
+        List<ButtonClicked> expected = Arrays.asList(btn1, btn2, btn3);
 
         assertEquals(expected, result);
     }
--- mobile-engage-sdk/src/main/java/com/emarsys/mobileengage/iam/model/specification/FilterByCampaignId.java
+++ mobile-engage-sdk/src/main/java/com/emarsys/mobileengage/iam/model/specification/FilterByCampaignId.java
@@ -2,21 +2,49 @@ package com.emarsys.mobileengage.iam.model.specification;
 
 import com.emarsys.core.database.repository.SqlSpecification;
 
+import java.util.Arrays;
+
 public class FilterByCampaignId implements SqlSpecification {
 
-    private final String campaignId;
+    private final String[] campaignIds;
+    private final String sql;
 
-    public FilterByCampaignId(String campaignId) {
-        this.campaignId = campaignId;
+    public FilterByCampaignId(String... campaignIds) {
+        this.campaignIds = campaignIds;
+        this.sql = createSql(campaignIds);
     }
 
     @Override
     public String getSql() {
-        return "campaign_id=?";
+        return sql;
     }
 
     @Override
     public String[] getArgs() {
-        return new String[]{campaignId};
+        return campaignIds;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FilterByCampaignId that = (FilterByCampaignId) o;
+
+        return Arrays.equals(campaignIds, that.campaignIds);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(campaignIds);
+    }
+
+    private String createSql(String[] args) {
+        StringBuilder sb = new StringBuilder("campaign_id IN (?");
+        for (int i = 1; i < args.length; i++) {
+            sb.append(", ?");
+        }
+        sb.append(")");
+        return sb.toString();
     }
 }
