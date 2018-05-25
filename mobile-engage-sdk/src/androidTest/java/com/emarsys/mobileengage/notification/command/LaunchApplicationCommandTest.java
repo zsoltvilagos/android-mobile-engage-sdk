4a90ed8d572c4c29c5b16d88c965bb6f0d8aac73
jpollak <9980358+pjozsef@users.noreply.github.com>
2018-05-25 16:28:23 +0200

    feat(launch-application-command): create command to launch app SUITEDEV-12357
    Co-authored-by: stoto <1409548+stoto@users.noreply.github.com>

---
+++ mobile-engage-sdk/src/androidTest/java/com/emarsys/mobileengage/notification/command/LaunchApplicationCommandTest.java
@@ -0,0 +1,68 @@
+package com.emarsys.mobileengage.notification.command;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.test.InstrumentationRegistry;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class LaunchApplicationCommandTest {
+
+    static {
+        mock(Activity.class);
+        mock(PackageManager.class);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor_intentMustNotBeNull() {
+        new LaunchApplicationCommand(null, InstrumentationRegistry.getTargetContext().getApplicationContext());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor_contextMustNotBeNull() {
+        new LaunchApplicationCommand(new Intent(), null);
+    }
+
+    @Test
+    public void testRun_startsActivity_withCorrectIntent() {
+        ArgumentCaptor<Intent> captor = ArgumentCaptor.forClass(Intent.class);
+
+        Intent launchIntentForPackage = new Intent();
+        PackageManager pm = mock(PackageManager.class);
+        when(pm.getLaunchIntentForPackage(anyString())).thenReturn(launchIntentForPackage);
+        Activity mockActivity = mock(Activity.class);
+        when(mockActivity.getPackageManager()).thenReturn(pm);
+        when(mockActivity.getPackageName()).thenReturn("packageName");
+
+        Bundle extras = new Bundle();
+        extras.putLong("key1", 800);
+        extras.putString("key2", "value");
+        Intent remoteIntent = new Intent();
+        remoteIntent.putExtras(extras);
+
+        Runnable command = new LaunchApplicationCommand(remoteIntent, mockActivity);
+
+        command.run();
+
+        verify(mockActivity).startActivity(captor.capture());
+
+        Bundle expectedBundle = launchIntentForPackage.getExtras();
+        Bundle resultBundle = captor.getValue().getExtras();
+        Assert.assertEquals(expectedBundle.keySet(), resultBundle.keySet());
+
+        for (String key : expectedBundle.keySet()) {
+            Assert.assertEquals(expectedBundle.get(key), resultBundle.get(key));
+        }
+    }
+
+}
