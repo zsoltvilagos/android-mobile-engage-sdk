01c4731818a3d38de37b8b6a3c247721110f61b3
jpollak <9980358+pjozsef@users.noreply.github.com>
2018-06-07 10:19:27 +0200

    feat(dependency-injection): introduce minimal dependency injection SUITEDEV-12537
    Co-authored-by: LaszloOriEmarsys <24588073+LaszloOriEmarsys@users.noreply.github.com>

---
+++ mobile-engage-sdk/src/main/java/com/emarsys/mobileengage/di/DependencyContainer.java
@@ -0,0 +1,24 @@
+package com.emarsys.mobileengage.di;
+
+import android.os.Handler;
+
+import com.emarsys.mobileengage.MobileEngageCoreCompletionHandler;
+import com.emarsys.mobileengage.MobileEngageInternal;
+import com.emarsys.mobileengage.RequestContext;
+import com.emarsys.mobileengage.deeplink.DeepLinkInternal;
+import com.emarsys.mobileengage.inbox.InboxInternal;
+
+public interface DependencyContainer {
+
+    MobileEngageInternal getMobileEngageInternal();
+
+    InboxInternal getInboxInternal();
+
+    DeepLinkInternal getDeepLinkInternal();
+
+    MobileEngageCoreCompletionHandler getCoreCompletionHandler();
+
+    Handler getCoreSdkHandler();
+
+    RequestContext getRequestContext();
+}
